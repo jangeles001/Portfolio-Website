9289f5abc0e9dfad63beda998b80d6d84c5958eb
jangeles001 <207365281+jangeles001@users.noreply.github.com>
2026-01-15 08:58:57 -0800

    Created website graphic for skills section

--- my-portfolio/src/sections/Hero.tsx
+++ my-portfolio/src/sections/Hero.tsx
@@ -5,6 +5,10 @@ export function HeroSection(){
                 <h1>Jaime Angeles</h1>
                 <img className='ml-auto' src={undefined}></img>
                 <p>Fullstack Developer focused on performance, clean UI, and maintainable code.</p>
+                <div className="flex justify-center gap-5">
+                    <a href="#contact-section">Contact Me</a>
+                    <a href="#project-section">Projects</a>
+                </div>
             </section>
         </div>
     )
